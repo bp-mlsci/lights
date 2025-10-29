ba62c9ebac6fe67fb6247d173c46dd337a4dd4d1
Bill Parkinson <bill_parkinson@yahoo.com>
2025-10-29 18:29:28 -0400

    initial project

---
+++ src/test/java/com/mlsci/lights/LightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlsci.lights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
